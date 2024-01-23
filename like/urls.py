ed0a14fdcbbc83a6149b14a03f4afbbf8226c0fa
Bai-ell <154439127+Bai-ell@users.noreply.github.com>
2024-01-23 17:03:58 +0600

    for_blog_ada

---
+++ like/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import LikeCreateView, LikeDeleteView
+
+urlpatterns = [
+    path('', LikeCreateView.as_view()),
+    path('<int:id>/', LikeDeleteView.as_view()),
+]
+
